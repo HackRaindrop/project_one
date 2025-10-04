3c3c70ba2520eedb2526e7f809625c64e1137143
HackRaindrop <jwkirk15@gmail.com>
2025-10-04 19:37:34 -0400

    Fix parseInt to use base 10

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,74 +1,72 @@
 const books = require('../data/books.json');
 
 const respondJSON = (request, response, status, object) => {
-    const content = JSON.stringify(object);
+  const content = JSON.stringify(object);
 
-    response.writeHead(status, {
-        'Content-Type': 'application/json',
-        'Content-Length': Buffer.byteLength(content, 'utf8'),
-    });
+  response.writeHead(status, {
+    'Content-Type': 'application/json',
+    'Content-Length': Buffer.byteLength(content, 'utf8'),
+  });
 
-    // Write content if not HEAD or 204
-    if (request.method !== 'HEAD' && status !== 204) {
-        response.write(content);
-    }
+  // Write content if not HEAD or 204
+  if (request.method !== 'HEAD' && status !== 204) {
+    response.write(content);
+  }
 
-    response.end();
+  response.end();
 };
 
 const getBooks = (request, response) => {
-    let result = books;
-    
-    // Check if user wants to limit total book results
-    const url = new URL(request.url, `http://${request.headers.host}`);
-    const limit = url.searchParams.get('limit');
-    
-    // If limit is specified, only send that many books
-    if (limit) {
-        const numLimit = parseInt(limit);
-        result = books.slice(0, numLimit);
-    }
-    
-    respondJSON(request, response, 200, result);
-}
+  let result = books;
+
+  // Check if user wants to limit total book results
+  const url = new URL(request.url, `http://${request.headers.host}`);
+  const limit = url.searchParams.get('limit');
+
+  // If limit is specified, only send that many books
+  if (limit) {
+    const numLimit = parseInt(limit, 10);
+    result = books.slice(0, numLimit);
+  }
+
+  respondJSON(request, response, 200, result);
+};
 
 const getAuthors = (request, response) => {
+  const authorNames = books.map((book) => book.author);
 
-    const authorNames = books.map(book => book.author);
-    
-    // Create set of unique authors and sort
-    const uniqueAuthors = [...new Set(authorNames)].sort();
-    
-    respondJSON(request, response, 200, uniqueAuthors);
-}
+  // Create set of unique authors and sort
+  const uniqueAuthors = [...new Set(authorNames)].sort();
+
+  respondJSON(request, response, 200, uniqueAuthors);
+};
 
 const getGenres = (request, response) => {
+  let allGenres = [];
+  books.forEach((book) => {
+    if (book.genres) {
+      allGenres = allGenres.concat(book.genres);
+    }
+  });
 
-    let allGenres = [];
-    books.forEach(book => {
-        if (book.genres) {
-            allGenres = allGenres.concat(book.genres);
-        }
-    });
-    
-    // Create set of unique genres and sort
-    const uniqueGenres = [...new Set(allGenres)].sort();
-    
-    respondJSON(request, response, 200, uniqueGenres);
-}
+  // Create set of unique genres and sort
+  const uniqueGenres = [...new Set(allGenres)].sort();
+
+  respondJSON(request, response, 200, uniqueGenres);
+};
 
 const getLanguages = (request, response) => {
-    const languageNames = books.map(book => book.language);
-    
-    // Create set of unique languages and sort
-    const uniqueLanguages = [...new Set(languageNames)].sort();
-    
-    respondJSON(request, response, 200, uniqueLanguages);
-}
+  const languageNames = books.map((book) => book.language);
+
+  // Create set of unique languages and sort
+  const uniqueLanguages = [...new Set(languageNames)].sort();
+
+  respondJSON(request, response, 200, uniqueLanguages);
+};
 
 module.exports = {
-    getBooks,
-    getAuthors,
-    getGenres,
-    getLanguages,
+  getBooks,
+  getAuthors,
+  getGenres,
+  getLanguages,
 };
--- src/server.js
+++ src/server.js
@@ -7,65 +7,65 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 // Parse body of request
 const parseBody = (request, response, handler) => {
-    const body = [];
+  const body = [];
 
-    // Handle errors    
-    request.on('error', (e) => {
-        console.dir(e);
-        response.statusCode = 400;
-        response.end();
-    });
+  // Handle errors
+  request.on('error', (e) => {
+    console.dir(e);
+    response.statusCode = 400;
+    response.end();
+  });
 
-    // Add chunk to body
-    request.on('data', (chunk) => {
-        body.push(chunk);
-    });
+  // Add chunk to body
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
 
-    // Parse body of request
-    request.on('end', () => {
-        const bodyString = Buffer.concat(body).toString();
-        request.body = JSON.parse(bodyString);
-        handler(request, response);
-    });
+  // Parse body of request
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    request.body = JSON.parse(bodyString);
+    handler(request, response);
+  });
 };
 
 // Handle POST requests
 const handlePost = (request, response, parsedURL) => {
-    if (parsedURL.pathname === '/addUser') {
-        parseBody(request, response, jsonResponses.addUser);
-    }
+  if (parsedURL.pathname === '/addUser') {
+    parseBody(request, response, jsonResponses.addUser);
+  }
 };
 
 // Handle GET requests
 const handleGet = (request, response, parsedURL) => {
-    switch (parsedURL.pathname) {
-        case '/style.css':
-            return htmlResponses.getCSS(request, response);
-        case '/':
-            return htmlResponses.getIndex(request, response);
-        case '/getBooks':
-            return jsonResponses.getBooks(request, response);
-        case '/getAuthors':
-            return jsonResponses.getAuthors(request, response);
-        case '/getGenres':
-            return jsonResponses.getGenres(request, response);
-        case '/getLanguages':
-            return jsonResponses.getLanguages(request, response);
-        default:
-            return htmlResponses.getIndex(request, response);
-    }
+  switch (parsedURL.pathname) {
+    case '/style.css':
+      return htmlResponses.getCSS(request, response);
+    case '/':
+      return htmlResponses.getIndex(request, response);
+    case '/getBooks':
+      return jsonResponses.getBooks(request, response);
+    case '/getAuthors':
+      return jsonResponses.getAuthors(request, response);
+    case '/getGenres':
+      return jsonResponses.getGenres(request, response);
+    case '/getLanguages':
+      return jsonResponses.getLanguages(request, response);
+    default:
+      return htmlResponses.getIndex(request, response);
+  }
 };
 
 // Handle all requests
 const onRequest = (request, response) => {
-    const protocol = request.connection.encrypted ? 'https:' : 'http';
-    const parsedURL = new URL(request.url, `${protocol}://${request.headers.host}`);
+  const protocol = request.connection.encrypted ? 'https:' : 'http';
+  const parsedURL = new URL(request.url, `${protocol}://${request.headers.host}`);
 
-    if (request.method === 'POST') {
-        handlePost(request, response, parsedURL);
-    } else {
-        handleGet(request, response, parsedURL);
-    }
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedURL);
+  } else {
+    handleGet(request, response, parsedURL);
+  }
 };
 
 // Create server
